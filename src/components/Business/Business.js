e7929412cb15b3c1e40153f4768922810a206869
eliseweir <67879336+eliseweir@users.noreply.github.com>
2020-11-30 23:20:29 -0600

    Reorganize files, add business & search bar comps

---
+++ src/components/Business/Business.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Business.css';
+
+const business = {
+
+};
+
+class Business extends React.Component{
+  render() {
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt='pizza photo'/>
+        </div>
+        <h2>Dominos</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>1010 Paddington Way</p>
+            <p>Bordertown</p>
+            <p>NY 10101</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>ITALIAN</h3>
+            <h3 className="rating">4.5 stars</h3>
+            <p>90 reviews</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Business;
